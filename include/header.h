a018b7753dc4c927f1754457fbfc1bbba1087069
William Wold <wm@wmww.sh>
2019-03-24 18:07:59 -0400

    Initial attempt

---
+++ include/header.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+void some_func();
+
+#endif // HEADER_H
---
+++ src/lib.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "header.h"
+
+void some_func() {
+    printf("some_func()\n");
+}
