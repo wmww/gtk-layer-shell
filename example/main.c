c3009b1ebc413af87f2667f337e3ae2453b4cc81
William Wold <wm@wmww.sh>
2019-04-05 11:47:39 -0400

    Expose layer setting in example UI

--- example/main.c
+++ example/main.c
@@ -21,6 +21,43 @@ on_button_press (GtkWidget *parent, GdkEventButton *event, void *_data)
 }
 
 static void
+on_layer_selected (GtkComboBox *widget,
+                   GtkWindow *layer_window)
+{
+    GtkComboBox *combo_box = widget;
+
+    if (gtk_combo_box_get_active (GTK_COMBO_BOX (combo_box)) != 0) {
+        gchar *layer = gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (combo_box));
+        if (g_strcmp0 (layer, "Background") == 0) {
+            gtk_window_set_layer_layer (layer_window, GTK_LAYER_SHELL_LAYER_BACKGROUND);
+        } else if (g_strcmp0 (layer, "Bottom") == 0) {
+            gtk_window_set_layer_layer (layer_window, GTK_LAYER_SHELL_LAYER_BOTTOM);
+        } else if (g_strcmp0 (layer, "Top") == 0) {
+            gtk_window_set_layer_layer (layer_window, GTK_LAYER_SHELL_LAYER_TOP);
+        } else if (g_strcmp0 (layer, "Overlay") == 0) {
+            gtk_window_set_layer_layer (layer_window, GTK_LAYER_SHELL_LAYER_OVERLAY);
+        } else {
+            g_assert_not_reached ();
+        }
+        g_free (layer);
+    }
+}
+
+
+static GtkWidget *
+layer_selection_new (GtkWindow *layer_window)
+{
+    GtkWidget *combo_box = gtk_combo_box_text_new ();
+    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), "Overlay");
+    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), "Top");
+    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), "Bottom");
+    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), "Background");
+    gtk_combo_box_set_active (GTK_COMBO_BOX (combo_box), 0);
+    g_signal_connect (combo_box, "changed", G_CALLBACK (on_layer_selected), layer_window);
+    return combo_box;
+}
+
+static void
 activate (GtkApplication* app, void *_data)
 {
     GtkWidget *window = gtk_application_window_new (app);
@@ -41,7 +78,7 @@ activate (GtkApplication* app, void *_data)
     GtkWidget *button = gtk_button_new_with_label ("Menu");
     g_signal_connect (button, "button_press_event",  G_CALLBACK (on_button_press), NULL);
     gtk_container_add (GTK_CONTAINER (vbox), button);
-    gtk_window_set_decorated (GTK_WINDOW (window), FALSE);
+    gtk_container_add (GTK_CONTAINER (vbox), layer_selection_new (GTK_WINDOW (window)));
     gtk_widget_show_all (window);
 }
 
