246bc8d51428ffa9ea1f30f0ec119a85b10f768a
William Wold <wm@wmww.sh>
2019-04-08 21:30:18 -0400

    Example refactoring

--- example/main.c
+++ example/main.c
@@ -20,42 +20,56 @@ on_button_press (GtkWidget *parent, GdkEventButton *event, void *_data)
     return TRUE;
 }
 
+struct {
+    const char *name;
+    GtkLayerShellLayer value;
+} const all_layers[] = {
+    {"Overlay", GTK_LAYER_OVERLAY},
+    {"Top", GTK_LAYER_TOP},
+    {"Bottom", GTK_LAYER_BOTTOM},
+    {"Background", GTK_LAYER_BACKGROUND},
+};
+
 static void
 on_layer_selected (GtkComboBox *widget, GtkWindow *layer_window)
 {
     GtkComboBox *combo_box = widget;
 
     gchar *layer = gtk_combo_box_text_get_active_text (GTK_COMBO_BOX_TEXT (combo_box));
-    if (g_strcmp0 (layer, "Background") == 0) {
-        gtk_layer_set_layer (layer_window, GTK_LAYER_BACKGROUND);
-    } else if (g_strcmp0 (layer, "Bottom") == 0) {
-        gtk_layer_set_layer (layer_window, GTK_LAYER_BOTTOM);
-    } else if (g_strcmp0 (layer, "Top") == 0) {
-        gtk_layer_set_layer (layer_window, GTK_LAYER_TOP);
-    } else if (g_strcmp0 (layer, "Overlay") == 0) {
-        gtk_layer_set_layer (layer_window, GTK_LAYER_OVERLAY);
-    } else {
-        g_assert_not_reached ();
+    gboolean layer_was_set = FALSE;
+    for (int i = 0; i < sizeof(all_layers) / sizeof(all_layers[0]); i++) {
+        if (g_strcmp0 (layer, all_layers[i].name) == 0) {
+            gtk_layer_set_layer (layer_window, all_layers[i].value);
+            layer_was_set = TRUE;
+            break;
+        }
     }
     g_free (layer);
+    g_return_if_fail (layer_was_set);
 }
 
 static GtkWidget *
-layer_selection_new (GtkWindow *layer_window)
+layer_selection_new (GtkWindow *layer_window, GtkLayerShellLayer starting_layer)
 {
-    GtkWidget *combo_box = gtk_combo_box_text_new ();
-    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), "Overlay");
-    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), "Top");
-    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), "Bottom");
-    gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), "Background");
-    g_signal_connect (combo_box, "changed", G_CALLBACK (on_layer_selected), layer_window);
-    GtkWidget *label = gtk_label_new ("Layer");
-    gtk_widget_set_halign (label, GTK_ALIGN_START);
-    GtkWidget *hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 6);
-    gtk_box_set_homogeneous (GTK_BOX (hbox), TRUE);
-    gtk_box_pack_start (GTK_BOX (hbox), label, TRUE, TRUE, 12);
-    gtk_box_pack_start (GTK_BOX (hbox), combo_box, TRUE, TRUE, 0);
-    return hbox;
+    GtkWidget *vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 6);
+    {
+        GtkWidget *label = gtk_label_new ("Layer:");
+        gtk_widget_set_halign (label, GTK_ALIGN_START);
+        gtk_box_pack_start (GTK_BOX (vbox), label, TRUE, TRUE, 12);
+    }{
+        GtkWidget *combo_box = gtk_combo_box_text_new ();
+        for (int i = 0; i < sizeof(all_layers) / sizeof(all_layers[0]); i++) {
+            gtk_combo_box_text_append_text (GTK_COMBO_BOX_TEXT (combo_box), all_layers[i].name);
+            if (all_layers[i].value == starting_layer)
+                gtk_combo_box_set_active (GTK_COMBO_BOX (combo_box), i);
+        }
+        g_signal_connect (combo_box, "changed", G_CALLBACK (on_layer_selected), layer_window);
+        gtk_box_pack_start (GTK_BOX (vbox), combo_box, TRUE, TRUE, 0);
+    }
+
+    gtk_layer_set_layer (layer_window, starting_layer);
+
+    return vbox;
 }
 
 typedef void (*AnchorEdgeSetter) (GtkWindow *window, gboolean anchor);
@@ -75,7 +89,7 @@ on_anchor_toggled (GtkButton *button, struct AnchorEdge *edge)
 }
 
 static void
-anchor_edge_new_for_button (GtkButton *button, GtkWindow *layer_window, AnchorEdgeSetter setter, gboolean anchor)
+anchor_edge_setup_button (GtkButton *button, GtkWindow *layer_window, AnchorEdgeSetter setter, gboolean anchor)
 {
     struct AnchorEdge *data = g_new0 (struct AnchorEdge, 1);
     *data = (struct AnchorEdge) {
@@ -97,35 +111,35 @@ anchor_control_new (GtkWindow *layer_window, gboolean left, gboolean right, gboo
 {
     GtkWidget *outside_hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
     {
-        GtkWidget *hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 0);
+        GtkWidget *hbox = gtk_box_new (GTK_ORIENTATION_HORIZONTAL, 5);
         gtk_box_pack_start (GTK_BOX (outside_hbox), hbox, TRUE, FALSE, 0);
         {
-            GtkWidget *left_vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
-            gtk_container_add (GTK_CONTAINER (hbox), left_vbox);
+            GtkWidget *vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+            gtk_container_add (GTK_CONTAINER (hbox), vbox);
             {
                 GtkWidget *button = gtk_button_new_from_icon_name ("go-first", GTK_ICON_SIZE_BUTTON);
-                gtk_box_pack_start (GTK_BOX (left_vbox), button, TRUE, FALSE, 0);
-                anchor_edge_new_for_button (GTK_BUTTON (button), layer_window, gtk_layer_set_anchor_left, left);
+                gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, FALSE, 0);
+                anchor_edge_setup_button (GTK_BUTTON (button), layer_window, gtk_layer_set_anchor_left, left);
             }
         }{
-            GtkWidget *center_vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+            GtkWidget *center_vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 16);
             gtk_container_add (GTK_CONTAINER (hbox), center_vbox);
             {
                 GtkWidget *button = gtk_button_new_from_icon_name ("go-top", GTK_ICON_SIZE_BUTTON);
                 gtk_box_pack_start (GTK_BOX (center_vbox), button, FALSE, FALSE, 0);
-                anchor_edge_new_for_button (GTK_BUTTON (button), layer_window, gtk_layer_set_anchor_top, top);
+                anchor_edge_setup_button (GTK_BUTTON (button), layer_window, gtk_layer_set_anchor_top, top);
             }{
                 GtkWidget *button = gtk_button_new_from_icon_name ("go-bottom", GTK_ICON_SIZE_BUTTON);
                 gtk_box_pack_end (GTK_BOX (center_vbox), button, FALSE, FALSE, 0);
-                anchor_edge_new_for_button (GTK_BUTTON (button), layer_window, gtk_layer_set_anchor_bottom, bottom);
+                anchor_edge_setup_button (GTK_BUTTON (button), layer_window, gtk_layer_set_anchor_bottom, bottom);
             }
         }{
-            GtkWidget *right_vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
-            gtk_container_add (GTK_CONTAINER (hbox), right_vbox);
+            GtkWidget *vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 0);
+            gtk_container_add (GTK_CONTAINER (hbox), vbox);
             {
                 GtkWidget *button = gtk_button_new_from_icon_name ("go-last", GTK_ICON_SIZE_BUTTON);
-                gtk_box_pack_start (GTK_BOX (right_vbox), button, TRUE, FALSE, 0);
-                anchor_edge_new_for_button (GTK_BUTTON (button), layer_window, gtk_layer_set_anchor_right, right);
+                gtk_box_pack_start (GTK_BOX (vbox), button, TRUE, FALSE, 0);
+                anchor_edge_setup_button (GTK_BUTTON (button), layer_window, gtk_layer_set_anchor_right, right);
             }
         }
     }
@@ -139,8 +153,6 @@ activate (GtkApplication* app, void *_data)
     GtkWidget *window = gtk_application_window_new (app);
 
     gtk_layer_init_for_window (GTK_WINDOW (window));
-    gtk_layer_set_anchor_left (GTK_WINDOW (window), TRUE);
-    gtk_layer_set_anchor_top (GTK_WINDOW (window), TRUE);
     gtk_layer_set_exclusive_zone (GTK_WINDOW (window), 20);
 
     gtk_window_set_default_size (GTK_WINDOW (window), -1, -1);
@@ -154,7 +166,7 @@ activate (GtkApplication* app, void *_data)
     GtkWidget *button = gtk_button_new_with_label ("Menu");
     g_signal_connect (button, "button_press_event",  G_CALLBACK (on_button_press), NULL);
     gtk_container_add (GTK_CONTAINER (vbox), button);
-    gtk_container_add (GTK_CONTAINER (vbox), layer_selection_new (GTK_WINDOW (window)));
+    gtk_container_add (GTK_CONTAINER (vbox), layer_selection_new (GTK_WINDOW (window), GTK_LAYER_TOP));
     gtk_container_add (GTK_CONTAINER (vbox), anchor_control_new (GTK_WINDOW (window), TRUE, FALSE, TRUE, FALSE));
     gtk_widget_show_all (window);
 }
