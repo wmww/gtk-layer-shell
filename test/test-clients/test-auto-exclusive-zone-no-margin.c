d8a114817b492dae053f1dab9d078da7fdec1dc1
William Wold <wm@wmww.sh>
2020-09-15 19:00:05 -0700

    Add auto exclusive zone test

--- test/test-clients/test-auto-exclusive-zone-no-margin.c
+++ test/test-clients/test-auto-exclusive-zone-no-margin.c
@@ -15,23 +15,58 @@ static GtkWindow* window;
 
 static void callback_0()
 {
+    // First, anchor to bottom
+    EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_exclusive_zone 200);
+
     window = create_default_window();
+
     gtk_layer_init_for_window(window);
-    gtk_layer_set_layer(window, GTK_LAYER_SHELL_LAYER_BOTTOM);
-    gtk_layer_set_namespace(window, "foobar");
+    gtk_layer_auto_exclusive_zone_enable(window);
     gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_BOTTOM, TRUE);
-    gtk_layer_set_exclusive_zone(window, 32);
+    gtk_widget_set_size_request(GTK_WIDGET(window), 300, 200);
+    gtk_widget_show_all(GTK_WIDGET(window));
 }
 
 static void callback_1()
 {
-    EXPECT_MESSAGE(zwlr_layer_shell_v1 .get_layer_surface 1 "foobar");
-    EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_exclusive_zone 32);
-    EXPECT_MESSAGE(wl_surface .commit);
-    gtk_widget_show_all(GTK_WIDGET(window));
+    // Resize
+    EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_exclusive_zone 240);
+
+    gtk_widget_set_size_request(GTK_WIDGET(window), 320, 240);
+    gtk_window_resize(window, 1, 1);
+}
+
+static void callback_2()
+{
+    // Streatch across left edge
+    EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_exclusive_zone 320);
+
+    gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_TOP, TRUE);
+    gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_LEFT, TRUE);
+}
+
+static void callback_3()
+{
+    // Stretch across top
+    EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_exclusive_zone 240);
+
+    gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_BOTTOM, FALSE);
+    gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_RIGHT, TRUE);
+}
+
+static void callback_4()
+{
+    // Anchor right
+    EXPECT_MESSAGE(zwlr_layer_surface_v1 .set_exclusive_zone 320);
+
+    gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_TOP, FALSE);
+    gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_LEFT, FALSE);
 }
 
 TEST_CALLBACKS(
     callback_0,
     callback_1,
+    callback_2,
+    callback_3,
+    callback_4,
 )
