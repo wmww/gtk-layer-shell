f3eb78ed9ae097a303f0013681de389849d1e20d
William Wold <wm@wmww.sh>
2020-10-24 16:45:03 -0700

    Add menu popup test

---
+++ test/test-clients/test-menu-popup.c
@@ -0,0 +1,41 @@
+/* This entire file is licensed under MIT
+ *
+ * Copyright 2020 William Wold
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "test-client-common.h"
+
+static GtkWindow* window;
+
+static void callback_0()
+{
+    // The mock server will automatically click on our window, triggering the menu to open
+
+    EXPECT_MESSAGE(zwlr_layer_shell_v1 .get_layer_surface);
+    EXPECT_MESSAGE(xdg_wm_base .get_xdg_surface);
+    EXPECT_MESSAGE(xdg_surface .get_popup);
+    EXPECT_MESSAGE(xdg_popup .grab);
+
+    window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
+    GtkWidget *menu_bar = gtk_menu_bar_new();
+    gtk_container_add(GTK_CONTAINER(window), menu_bar);
+    GtkWidget *menu_item = gtk_menu_item_new_with_label("Popup menu");
+    gtk_container_add(GTK_CONTAINER(menu_bar), menu_item);
+    GtkWidget *submenu = gtk_menu_new();
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(menu_item), submenu);
+    GtkWidget *close_item = gtk_menu_item_new_with_label("Menu item");
+    gtk_menu_shell_append(GTK_MENU_SHELL(submenu), close_item);
+
+    gtk_layer_init_for_window(window);
+    gtk_widget_show_all(GTK_WIDGET(window));
+}
+
+TEST_CALLBACKS(
+    callback_0,
+)
