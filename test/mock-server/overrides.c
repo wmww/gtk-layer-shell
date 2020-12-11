23895bfb3aa7315bbe53b202b6f40224d107239d
William Wold <wm@wmww.sh>
2020-12-10 20:21:18 -0800

    Mock server: support subsurfaces

--- test/mock-server/overrides.c
+++ test/mock-server/overrides.c
@@ -331,6 +331,7 @@ void init()
     default_global_create(display, &wl_shm_interface, 1);
     default_global_create(display, &wl_data_device_manager_interface, 2);
     default_global_create(display, &wl_compositor_interface, 4);
+    default_global_create(display, &wl_subcompositor_interface, 1);
     default_global_create(display, &xdg_wm_base_interface, 2);
     default_global_create(display, &zwlr_layer_shell_v1_interface, 3);
 }
