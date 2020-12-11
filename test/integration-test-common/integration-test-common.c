535c6e8d00aba71673a252229cdd5dd5dcb9c185
William Wold <wm@wmww.sh>
2020-12-10 19:40:58 -0800

    Integration tests add debug control window because I gotta be extra

--- test/integration-test-common/integration-test-common.c
+++ test/integration-test-common/integration-test-common.c
@@ -46,11 +46,44 @@ GtkWindow* create_default_window()
     return window;
 }
 
-int main()
+static void continue_button_callback(GtkWidget *_widget, gpointer _data)
+{
+    (void)_widget; (void)_data;
+    next_step(NULL);
+}
+
+static void create_debug_control_window()
+{
+    // Make a window with a continue button for debugging
+    GtkWindow* window = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
+    gtk_layer_init_for_window(window);
+    gtk_layer_set_anchor(window, GTK_LAYER_SHELL_EDGE_BOTTOM, TRUE);
+    gtk_layer_set_margin(window, GTK_LAYER_SHELL_EDGE_BOTTOM, 200);
+    gtk_layer_set_layer(window, GTK_LAYER_SHELL_LAYER_OVERLAY);
+    GtkWidget* button = gtk_button_new_with_label("Continue ->");
+    g_signal_connect (button, "clicked", G_CALLBACK(continue_button_callback), NULL);
+    gtk_container_add(GTK_CONTAINER(window), button);
+    gtk_widget_show_all(GTK_WIDGET(window));
+    // This will only be called once, so leaking the window is fine
+}
+
+int main(int argc, char** argv)
 {
     gtk_init(0, NULL);
-    next_step(NULL);
-    g_timeout_add(STEP_TIME, next_step, NULL);
+
+    if (argc == 1) {
+        // No args except the program itself, run the normal test
+        next_step(NULL);
+        g_timeout_add(step_time, next_step, NULL);
+    } else if (argc == 2 && g_strcmp0(argv[1], "--debug") == 0) {
+        // Run with a debug mode window that lets the user advance manually
+        create_debug_control_window();
+        next_step(NULL);
+    } else {
+        g_critical("Invalid arguments to integration test");
+        return 1;
+    }
+
     gtk_main();
     return return_code;
 }
