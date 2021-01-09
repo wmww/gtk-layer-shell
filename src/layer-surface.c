7431c50378018276fa16ac7e3c333bb968f93668
Daniel Kondor <kondor.dani@gmail.com>
2021-01-09 12:47:32 +0800

    Fix crash if LayerSurface does not have a Wayland object yet

--- src/layer-surface.c
+++ src/layer-surface.c
@@ -433,7 +433,8 @@ layer_surface_auto_exclusive_zone_enable (LayerSurface *self)
 void
 layer_surface_set_keyboard_interactivity (LayerSurface *self, GtkLayerShellKeyboardInteractivity interactivity)
 {
-    uint32_t version = zwlr_layer_surface_v1_get_version (self->layer_surface);
+    uint32_t version = self->layer_surface ? zwlr_layer_surface_v1_get_version (self->layer_surface)
+		: gtk_layer_get_layer_shell_version();
     if (version <= 3 && interactivity == GTK_LAYER_SHELL_KEYBOARD_ON_DEMAND)
     {
         g_warning ("Requested keyboard interactivity setting not supported by the compositor!");
